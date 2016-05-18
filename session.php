2f1b3f2065c51b625f3a34fb0fbcafbc35c86a84
virender85 <viru.sing6@gmail.com>
2016-05-18 10:35:32 +0530

    Create session.php

---
+++ session.php
@@ -0,0 +1,15 @@
+<?php
+$sessions = array();
+
+$path = realpath(session_save_path());
+$files = array_diff(scandir($path), array('.', '..'));
+
+foreach ($files as $file)
+{
+    $sessions[$file] = unserialize(file_get_contents($path . '/' . $file));
+}
+
+echo '<pre>';
+print_r($sessions);
+echo '</pre>';
+?>
